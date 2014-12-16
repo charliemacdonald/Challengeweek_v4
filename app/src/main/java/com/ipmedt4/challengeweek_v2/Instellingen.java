a46edaf11535805b533ac0d0efc6cec52f6d50a4
charliemacdonald <charlenemacdonald@live.nl>
2014-12-16 12:45:48 +0100

    instellingenscherm, versiescherm, disclaimerscherm en uitloggen aangemaakt

--- app/src/main/java/com/ipmedt4/challengeweek_v2/Instellingen.java
+++ app/src/main/java/com/ipmedt4/challengeweek_v2/Instellingen.java
@@ -1,17 +1,54 @@
 package com.ipmedt4.challengeweek_v2;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class Instellingen extends ActionBarActivity {
+Button versiebutton;
+Button disclaimerbutton;
+Button uitlogbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instellingen);
+        Button versiebutton = (Button) findViewById(R.id.versiebutton);
+        Button disclaimerbutton = (Button) findViewById(R.id.disclaimerbutton);
+        Button uitlogbutton = (Button) findViewById(R.id.uitlogbutton);
+        versiebutton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent intent = new Intent(v.getContext(), Versiescherm.class);
+                startActivityForResult(intent, 0);
+
+
+            }});
+
+        disclaimerbutton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent myintent2 = new Intent(v.getContext(), Disclaimerscherm.class);
+                startActivity(myintent2);
+
+            }
+        });
+
+        uitlogbutton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent myintent2 = new Intent(v.getContext(), Loginscherm.class);
+                startActivity(myintent2);
+
+            }
+        });
     }
 
 
